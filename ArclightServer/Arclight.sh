fbee8880761397edac314d55b1f333af771fc73d
AdamOnBlox <adamonroblox@outlook.com>
2024-07-27 13:58:04 +0000

    first commit

---
+++ ArclightServer/Arclight.sh
@@ -0,0 +1,2 @@
+java -Xmx6G -Xms2G -jar /workspaces/codespaces-blank/ArclightServer/arclight.jar nogui
+
---
+++ PaperServer/Paper.sh
@@ -0,0 +1,1 @@
+java -Xmx6G -Xms2G -jar /workspaces/codespaces-blank/PaperServer/Paper.jar nogui
